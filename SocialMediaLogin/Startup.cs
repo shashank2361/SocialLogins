f6b06c15dca785f458dccbbf6a112710d4bee7db
DESKTOP-SB3UMDG\Shashank-PC <Shashank2361@yahoo.co.in>
2018-09-24 15:17:35 +0100

    First Commit

---
+++ SocialMediaLogin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SocialMediaLogin.Startup))]
+namespace SocialMediaLogin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
